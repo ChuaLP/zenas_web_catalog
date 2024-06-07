bc0052ead167ab07536305c42e70a2fbf638faba
ChuaLP <49647591+ChuaLP@users.noreply.github.com>
2024-06-07 14:14:02 +0800

    Update zenas_web_catalog.py

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -19,7 +19,7 @@ df = pandas.DataFrame(my_catalog)
 
 # put the first column into a list
 color_list = df[0].values.tolist()
-print(color_list)
+#print(color_list)
 
 # Let's put a pick list here so they can pick the color
 option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
@@ -30,15 +30,15 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
 #my_cur.execute(",select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
 #color_or_style = '"+option+"';")
-#my_cur.execute(""select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style='pink'";")
+my_cur.execute(""select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style='pink'";")
 #df2 = my_cur.fetchone()
 
-#streamlit.image(
- # df2[0],
-#  width=400,
- # caption= product_caption
-# )
+streamlit.image(
+  df2[0],
+  width=400,
+ caption= product_caption
+ )
 
-#streamlit.write('Price: ', df2[1])
-#streamlit.write('Sizes Available: ',df2[2])
-#streamlit.write(df2[3])
+streamlit.write('Price: ', df2[1])
+streamlit.write('Sizes Available: ',df2[2])
+streamlit.write(df2[3])
