503aa7df9956ffaf6e7a251237796be2e4c018ff
ChuaLP <49647591+ChuaLP@users.noreply.github.com>
2024-06-07 14:04:50 +0800

    Update zenas_web_catalog.py

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -30,8 +30,8 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
 #my_cur.execute(",select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
 #color_or_style = '"+option+"';")
-my_cur.execute(""select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style='pink'";")
-df2 = my_cur.fetchone()
+#my_cur.execute(""select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style='pink'";")
+#df2 = my_cur.fetchone()
 
 #streamlit.image(
   df2[0],
@@ -39,6 +39,6 @@ df2 = my_cur.fetchone()
   caption= product_caption
  )
 
-streamlit.write('Price: ', df2[1])
-streamlit.write('Sizes Available: ',df2[2])
-streamlit.write(df2[3])
+#streamlit.write('Price: ', df2[1])
+#streamlit.write('Sizes Available: ',df2[2])
+#streamlit.write(df2[3])
